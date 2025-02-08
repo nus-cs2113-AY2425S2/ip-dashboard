532a88816267e6fbcf0bf7a5d8030f82530ad7c0
Damith C. Rajapakse <1673303+damithc@users.noreply.github.com>
2025-02-08 12:39:44 +0800

    Update run.sh: Switch to using the latest release

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
-./get-reposense.py --master
+./get-reposense.py --release
 java -jar RepoSense.jar --config ./configs --since 10/01/2025  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
